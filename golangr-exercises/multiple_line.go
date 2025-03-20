18b140be7d779777130e7818f66a283d4582e9ca
Davidnbr <davidnbr_98@hotmail.com>
2025-03-19 19:06:20 -0500

    Add: New

---
+++ golangr-exercises/multiple_line.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	str1 := "This is a string variable!"
+
+	// 1: Function calls
+	fmt.Println(str1)
+	fmt.Println(str1)
+
+	// 2: use newline character
+	fmt.Println("Hello world \n I am Go")
+}
